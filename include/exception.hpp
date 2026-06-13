3834f5e4abb37dc40025bdedcc6ee218e3c96ce9
naoto <naotofujiwara890@gmail.com>
2026-06-13 23:08:19 +0900

    packedをC++11の機能を使って揃える

--- include/exception.hpp
+++ include/exception.hpp
@@ -3,13 +3,13 @@
 namespace exception
 {
 
-struct register_state_t
+struct [[gnu::packed]] register_state_t
 {
     uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
     uint64_t rbp, rdi, rsi, rdx, rcx, rbx, rax;
     uint64_t int_no, err_code;
     uint64_t rip, cs, rflags, rsp, ss;
-} __attribute__((packed));
+};
 
 // isr.asm から呼ばれるため C リンケージ (名前マングリング無効)
 extern "C" void isr_common_handler(register_state_t *regs);
